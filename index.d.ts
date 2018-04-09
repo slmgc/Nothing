1cc0d0284b77bb0f5a249a57227b3a0fb4aeb2cf
slmgc <slmgc@ya.ru>
2018-04-08 20:30:13 -0700

    Release 1.0.2

--- index.d.ts
+++ index.d.ts
@@ -1,32 +1,6 @@
-interface INothing {
-	(): INothing
-	(p1: any): INothing
-	(p1: any, p2: any): INothing
-	(p1: any, p2: any, p3: any): INothing
-	(p1: any, p2: any, p3: any, p4: any): INothing
-	(p1: any, p2: any, p3: any, p4: any, p5: any): INothing
-	(p1: any, p2: any, p3: any, p4: any, p5: any, p6: any): INothing
-	(p1: any, p2: any, p3: any, p4: any, p5: any, p6: any, p7: any): INothing
-	(p1: any, p2: any, p3: any, p4: any, p5: any, p6: any, p7: any, p8: any): INothing
-	(p1: any, p2: any, p3: any, p4: any, p5: any, p6: any, p7: any, p8: any, p9: any): INothing
-	(p1: any, p2: any, p3: any, p4: any, p5: any, p6: any, p7: any, p8: any, p9: any, p10: any): INothing
-	[key: string]: INothing
-}
-
-interface IFunctionProps {
-	length: 0
-	name: string
-	prototype: object
-	toLocaleString(): ''
-	toString(): ''
-	valueOf(): false
-}
-
-declare const Nothing: IFunctionProps & INothing
-declare function toBool <T>(p: T): T extends INothing ? false : boolean
-declare function isNothing <T>(p: T): T extends INothing ? true : false
-declare function isSomething <T>(p: T): T extends INothing | null | void ? false : true
-declare function serialize (p: object): string
-declare function deserialize (p: string): object
-
-export {Nothing, toBool, isNothing, isSomething, serialize, deserialize}
+export declare const Nothing: any
+export declare function toBool (p: any): boolean
+export declare function isNothing (p: any): boolean
+export declare function isSomething (p: any): boolean
+export declare function serialize (p: object): string
+export declare function deserialize (p: string): object
